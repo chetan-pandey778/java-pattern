808fd5b7a7e6e7702400fe55d6bc4d8930823647
Chetan pandey <114645067+chetan-pandey778@users.noreply.github.com>
2023-07-26 12:30:05 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,14 @@
+ class Demo{
+    int a=10; String b=chetan;
+    void show(){
+        System.out.println(a+" "+b);
+    }
+    Class Test
+    {
+        Public Static void main(String[] args){
+            Demo r = new Demo();
+            r.show();
+        }
+
+    }
+}
